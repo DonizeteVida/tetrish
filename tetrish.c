0f3de45bcc365703d8b1e955f32b4a4558e02aa9
DonizeteVida <donizetevida1998@outlook.com>
2025-01-28 00:36:28 -0300

    Single bit manipulation is working

--- tetrish.c
+++ tetrish.c
@@ -6,10 +6,13 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+typedef uint16_t Row;
+
 struct Board
 {
-    uint8_t size;
-    uint8_t rows[20];
+    uint8_t x;
+    uint8_t y;
+    Row rows[20];
 };
 
 struct Input
@@ -19,6 +22,8 @@ struct Input
 
 struct Piece
 {
+    uint8_t x;
+    uint8_t y;
     uint8_t rotation : 2;
 };
 
@@ -35,10 +40,10 @@ handle_render(struct Game *game)
     while (true)
     {
         system("clear");
-        for (int8_t y = 0; y < game->board->size >> 4; y++)
+        for (int8_t y = 0; y < game->board->y; y++)
         {
             printf("<!");
-            for (int8_t x = game->board->size & 0x0F; x > 0; x--)
+            for (int8_t x = game->board->x; x > 0; x--)
             {
                 if (game->board->rows[y] >> (x - 1) & 0x01)
                 {
@@ -65,38 +70,63 @@ static void handle_input(struct Game *game)
     {
         switch (c)
         {
-        case 'a':
-            game->input->direction = 0b10;
-            break;
-
-        case 'b':
-            game->input->direction = 0b01;
-            break;
+            case 'a': { game->input->direction = 0b10; break; }
+            case 'd': { game->input->direction = 0b01; break; }
         }
     }
 }
 
 static void handle_piece(struct Game *game)
 {
+    struct Input *input = game->input;
+    struct Piece *piece = game->piece;
     while (true)
     {
+        {
+            Row x_offset = (int)(sizeof(Row) * 8) - piece->x - 1;
+            Row row = game->board->rows[piece->y] & ~(1 << x_offset);
+            game->board->rows[piece->y] = row;
+        }
 
+        piece->y++;
+
+        switch (input->direction)
+        {
+            case 0b10: { piece->x--; break; }
+            case 0b01: { piece->x++; break; }
+        }
+
+        {
+            Row x_offset = (int)(sizeof(Row) * 8) - piece->x - 1;
+            Row row = game->board->rows[piece->y] | (1 << x_offset);
+            game->board->rows[piece->y] = row;
+        }
+
+        game->input->direction = 0;
         sleep(1);
     }
 }
 
-void main(int argc, char **argv)
+int main(int argc, char **argv)
 {
     struct Board board = {
-        .size = 0b10001000,
+        .x = sizeof(Row) * 8,
+        .y = 20,
         .rows = {0},
     };
+
+    board.rows[0] = 0b1000000000000001;
+    board.rows[1] = 0b1000000000000001;
+    board.rows[2] = 0b1000000000000001;
+    board.rows[3] = 0b1000000000000001;
 
     struct Input input = {
         .direction = 0,
     };
 
     struct Piece piece = {
+        .x = 15,
+        .y = 0,
         .rotation = 0,
     };
 
